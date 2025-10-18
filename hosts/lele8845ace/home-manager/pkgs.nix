cdd2b32b782548a945a65a87aab99ed8a44f2c8d
Emanuele "Lele" Calo <emanuele.lele.calo@gmail.com>
2025-10-19 00:23:23 +0200

    nix: fix packages

--- hosts/lele8845ace/home-manager/pkgs.nix
+++ hosts/lele8845ace/home-manager/pkgs.nix
@@ -1,31 +1,14 @@
 {
-  inputs,
   pkgs,
   ...
 }:
-let
-  system = "x86_64-linux";
-in
 {
   home = {
-    packages =
-      (with pkgs; [
-        barrier
-        dbeaver-bin
-        guvcview
-        quickemu
-        remmina
-        spice
-        uqm
-        uvcdynctrl
-        vlc
-        wine
-      ])
-      ++ (with pkgs.unstable; [
+    packages = (
+      with pkgs.unstable;
+      [
         #davinci-resolve-studio
-      ])
-      ++ ([
-        inputs.zen-browser.packages."${system}".specific
-      ]);
+      ]
+    );
   };
 } # EOF
