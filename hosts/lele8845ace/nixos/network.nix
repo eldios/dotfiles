4b2f9f79757c5635cf258979cf2ce04486e6ad7c
Emanuele "Lele" Calo <emanuele.lele.calo@gmail.com>
2025-10-28 04:33:01 +0100

    lele8845ace: update config

--- hosts/lele8845ace/nixos/network.nix
+++ hosts/lele8845ace/nixos/network.nix
@@ -1,12 +1,34 @@
-{ lib, ... }:
 {
+  # Rename interfaces based on MAC address to predictable names
+  systemd.network.links = {
+    "10-eno0" = {
+      matchConfig.MACAddress = "10:ff:e0:3d:a0:7d";
+      linkConfig.Name = "eno0";
+    };
+    "10-wlan0" = {
+      matchConfig.MACAddress = "e8:47:3a:e7:f6:59";
+      linkConfig.Name = "wlan0";
+    };
+  };
+
   networking = {
-    networkmanager.enable = true;
+    usePredictableInterfaceNames = false; # We handle naming via systemd.network.links
+    networkmanager = {
+      enable = true;
+      unmanaged = [ ];
+    };
 
-    useDHCP = lib.mkDefault true;
-    dhcpcd.enable = true;
+    bridges = {
+      br0 = {
+        interfaces = [ "eno0" ];
+      };
+    };
 
-    interfaces = { };
+    interfaces = {
+      br0 = {
+        useDHCP = true;
+      };
+    };
 
     hostName = "lele8845ace";
     hostId = "d34d0008"; # random chars
@@ -17,6 +39,9 @@
       allowedTCPPorts = [
         24800 # default Barrier KVM software port
       ];
+
+      checkReversePath = false;
+      trustedInterfaces = [ "br0" ];
     };
 
   };
