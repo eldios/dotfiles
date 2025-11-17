7dccf89bfc10bb5c2251392dbe0ca0c693aafaa9
Emanuele "Lele" Calo <emanuele.lele.calo@gmail.com>
2025-11-17 17:42:11 +0100

    nix-cache: add local nix cache

--- hosts/sox1x/nixos/configuration.nix
+++ hosts/sox1x/nixos/configuration.nix
@@ -1,57 +1,65 @@
-{ inputs, nixpkgs, nixpkgs-unstable, nixos-hardware, home-manager, ... }:
+{
+  inputs,
+  nixpkgs,
+  nixpkgs-unstable,
+  nixos-hardware,
+  home-manager,
+  ...
+}:
 
 {
-  imports =
-    [
-      # select hardware from https://github.com/NixOS/nixos-hardware/blob/master/flake.nix
-      nixos-hardware.nixosModules.lenovo-thinkpad-x1-extreme-gen2
-      nixos-hardware.nixosModules.common-cpu-intel
-      nixos-hardware.nixosModules.common-gpu-nvidia
-      nixos-hardware.nixosModules.common-pc-laptop-ssd
+  imports = [
+    # select hardware from https://github.com/NixOS/nixos-hardware/blob/master/flake.nix
+    nixos-hardware.nixosModules.lenovo-thinkpad-x1-extreme-gen2
+    nixos-hardware.nixosModules.common-cpu-intel
+    nixos-hardware.nixosModules.common-gpu-nvidia
+    nixos-hardware.nixosModules.common-pc-laptop-ssd
 
-      ../../../common/nixos/sops.nix
+    ../../../common/nixos/sops.nix
 
-      ../../../common/nixos/locale.nix
-      ../../../common/nixos/locale_gui.nix
+    ../../../common/nixos/locale.nix
+    ../../../common/nixos/locale_gui.nix
 
-      ../../../common/nixos/users.nix
-      ../../../common/nixos/system.nix
+    ../../../common/nixos/users.nix
+    ../../../common/nixos/system.nix
 
-      ../../../common/nixos/programs/neovim.nix
+    ../../../common/nixos/nix-cache.nix
 
-      ../../../common/nixos/virtualisation.nix
+    ../../../common/nixos/programs/neovim.nix
 
-      ./hardware-configuration.nix
+    ../../../common/nixos/virtualisation.nix
 
-      ./boot.nix
-      ./system.nix
-      ./network.nix
-      ./users.nix
+    ./hardware-configuration.nix
 
-      home-manager.nixosModules.home-manager
-      {
-        home-manager.backupFileExtension = "hm-backup";
+    ./boot.nix
+    ./system.nix
+    ./network.nix
+    ./users.nix
 
-        home-manager.users.eldios = import ../home-manager/home.nix;
+    home-manager.nixosModules.home-manager
+    {
+      home-manager.backupFileExtension = "hm-backup";
 
-        home-manager.sharedModules = [
-          inputs.sops-nix.homeManagerModules.sops
-          inputs.stylix.homeModules.stylix
-        ];
+      home-manager.users.eldios = import ../home-manager/home.nix;
 
-        home-manager.useGlobalPkgs = true;
-        home-manager.useUserPackages = true;
-        home-manager.extraSpecialArgs = {
-          inherit
-            inputs
-            nixpkgs
-            nixpkgs-unstable
-            home-manager
-            nixos-hardware
-            ;
-        };
-      }
-    ];
+      home-manager.sharedModules = [
+        inputs.sops-nix.homeManagerModules.sops
+        inputs.stylix.homeModules.stylix
+      ];
+
+      home-manager.useGlobalPkgs = true;
+      home-manager.useUserPackages = true;
+      home-manager.extraSpecialArgs = {
+        inherit
+          inputs
+          nixpkgs
+          nixpkgs-unstable
+          home-manager
+          nixos-hardware
+          ;
+      };
+    }
+  ];
 }
 
 # vim: set ts=2 sw=2 et ai list nu
