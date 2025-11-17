c27150d11957bf0e751d7ad668965887904ade03
Emanuele "Lele" Calo <emanuele.lele.calo@gmail.com>
2025-11-17 17:40:17 +0100

    minixos: update kernel

--- hosts/mininixos/nixos/boot.nix
+++ hosts/mininixos/nixos/boot.nix
@@ -7,8 +7,7 @@
 
     supportedFilesystems = [ "zfs" ];
 
-    #kernelPackages = pkgs.linuxPackages_latest;
-    kernelPackages = pkgs.linuxPackages_6_16;
+    kernelPackages = pkgs.linuxPackages_6_17;
     kernelParams = [
       "nohibernate"
       "zfs.zfs_arc_max=6442856000"
