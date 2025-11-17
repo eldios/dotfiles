935e27ddbb31cb011ff11bc95f042853d6eed896
Emanuele "Lele" Calo <emanuele.lele.calo@gmail.com>
2025-11-17 01:16:54 +0100

    nix-cache more substituers: enable for home-manager

--- common/nixos/nix-cache.nix
+++ common/nixos/nix-cache.nix
@@ -1,9 +1,15 @@
 {
   nix.settings = {
+    experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
+
     # Try local cache first, then fallback to upstream
     substituters = [
       "https://nix-cache.casa.lele.rip" # Local NCPS cache
       "https://cache.nixos.org" # Upstream fallback
+      "https://nix-community.cachix.org"
     ];
 
     # Trust the NCPS signing key
@@ -11,6 +17,7 @@
     trusted-public-keys = [
       "nix-cache.casa.lele.rip:pXEMvYrY/AgTKBw7A1d4DZi8YRbjBby3eMxnmEo3Aqs="
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
   };
 }
