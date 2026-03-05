7072f5b96ab8c2fd9df60c32dfa731c1ec372400
Emanuele "Lele" Calo <emanuele.lele.calo@gmail.com>
2026-03-05 22:59:33 +0100

    mininixos: add kla VM

--- hosts/mininixos/nixos/libvirt-vms.nix
+++ hosts/mininixos/nixos/libvirt-vms.nix
@@ -68,6 +68,20 @@
         ExecStop = "${pkgs.libvirt}/bin/virsh shutdown bsdino";
       };
     };
+
+    # kla VM (Arch Linux - OpenClaw)
+    "libvirt-vm-kla" = {
+      description = "Libvirt VM: kla";
+      after = [ "libvirtd.service" ];
+      wantedBy = [ "multi-user.target" ];
+
+      serviceConfig = {
+        Type = "oneshot";
+        RemainAfterExit = "yes";
+        ExecStart = "${pkgs.bash}/bin/bash -c \"state=$(${pkgs.libvirt}/bin/virsh domstate kla 2>/dev/null || echo 'shut off'); [[ $state == 'shut off' ]] && ${pkgs.libvirt}/bin/virsh start kla || exit 0\"";
+        ExecStop = "${pkgs.libvirt}/bin/virsh shutdown kla";
+      };
+    };
   };
 }
 
