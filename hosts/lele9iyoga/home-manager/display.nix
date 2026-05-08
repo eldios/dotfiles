d10b54d639fe347abdb78fb8f22aef60e72e33a5
Emanuele "Lele" Calo <emanuele.lele.calo@gmail.com>
2026-05-08 19:52:12 +0200

    fix(lele9iyoga): drop pointerCursor size 48 -> 32
    48 felt oversized at scale 1.75; 32 more proportional.

--- hosts/lele9iyoga/home-manager/display.nix
+++ hosts/lele9iyoga/home-manager/display.nix
@@ -30,7 +30,7 @@
   home.pointerCursor = {
     package = pkgs.bibata-cursors;
     name = "Bibata-Modern-Classic";
-    size = 48;
+    size = 32;
     gtk.enable = true;
     x11.enable = true;
   };
