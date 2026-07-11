b90893c9c50fb2e971487f4841a16707f40e680f
Emanuele "Lele" Calo <emanuele.lele.calo@gmail.com>
2026-07-11 04:01:37 +0200

    feat(git): add signature status to all logs formats

--- common/home-manager/eldios/programs/git.nix
+++ common/home-manager/eldios/programs/git.nix
@@ -52,8 +52,8 @@ in
         alias = {
           co = "checkout";
           chp = "cherry-pick";
-          lol = "log --graph --abbrev-commit --decorate --date=relative --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all";
-          loll = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n'' %C(white)%s%C(reset) %C(dim white)- %an%C(reset)' --all";
+          lol = "log --graph --abbrev-commit --decorate --date=relative --format=format:'%C(bold blue)%h%C(reset) %C(bold magenta)%G?%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all";
+          loll = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) %C(bold magenta)%G?%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n'' %C(white)%s%C(reset) %C(dim white)- %an%C(reset)' --all";
           ppt = "pull --prune --tags";
           rc = "repo clone";
           st = "status";
